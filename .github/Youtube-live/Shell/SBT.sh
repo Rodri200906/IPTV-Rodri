a0a395e0e088359428c51f88f6bff0c8665cfec1
Rodri200906 <131870845+Rodri200906@users.noreply.github.com>
2024-10-21 20:46:56 +0200

    Delete .github/Youtube-live directory

--- .github/Youtube-live/Shell/SBT.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo $(dirname $0)
-
-python3 -m pip install requests
-
-python3 .github/Youtube-live/Python/SBT.py > .github/Youtube-live/channels-m3u8/SBT-live.m3u8
-
-echo "The m3u8 link is extracted."
