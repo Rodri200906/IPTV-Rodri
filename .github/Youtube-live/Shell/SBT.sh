3af58cc2c27478ea0cb74765ae853390a7921feb
Rodri200906 <131870845+Rodri200906@users.noreply.github.com>
2024-10-21 18:05:46 +0200

    Update SBT.sh

--- .github/Youtube-live/Shell/SBT.sh
+++ .github/Youtube-live/Shell/SBT.sh
@@ -4,8 +4,6 @@ echo $(dirname $0)
 
 python3 -m pip install requests
 
-cd $(dirname $0)../Python/
-
-python3 SBT.py > ../channels-m3u8/SBT-Live.m3u8
+python3 .github/Youtube-live/Shell/SBT.sh > .github/Youtube-live/channels-m3u8/SBT-live.m3u8
 
 echo "The m3u8 link is extracted."
